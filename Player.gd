ebd3e8bd4bc23a6a7c430057e30399f9ae6bbd5e
Keith Oshima <koshima789@gmail.com>
2022-10-29 23:50:38 -0700

    First commit with godot assets.

---
+++ Player.gd
@@ -0,0 +1,16 @@
+extends KinematicBody2D
+
+
+func _ready() -> void:
+	pass
+	
+
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("up"):
+		pass
+	if Input.is_action_pressed("down"):
+		pass
+	if Input.is_action_pressed("left"):
+		pass
+	if Input.is_action_pressed("right"):
+		pass
